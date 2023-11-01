001395c4d93629b2d1aba69ded415518aa0d783c
0xissam <hah305581@gmail.com>
2023-11-02 00:02:58 +0100

    update

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -1,32 +1,32 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_heap - checks  dfgreif a binary tree is a valid Max Binary Heap.
- * @tree: pointer to the fre root node of the tree to check.
+ * binary_tree_is_heap - csds if a binasdry tree is a valid Max Binary Heap.
+ * @tree: pointer to the rooode of the tree to check.
  *
- * Return: 1 if tree is fgdg a valid Max Binary Heap, and 0 otherwise
+ * Return: 1 if tree is a vasMax Binary Heap, and 0 otherwise
  */
 int binary_tree_is_heap(const binary_tree_t *tree)
 {
-	
+	/* If the tree issdfpty, return 0. */
 	if (!tree)
 		return (0);
 
-	
+	/* If the tree has no chilsdfn, return 1. */
 	if (!tree->left && !tree->right)
 		return (1);
 
-	
+	/* If the tree has children, */
 	if (tree->left && tree->right)
 	{
-		
+		/* check if the parent is gresd thansdth children. */
 		if (tree->n >= tree->left->n && tree->n >= tree->right->n)
 
-			
+			/* If so, recursdly check the left and right subtrees. */
 			return (binary_tree_is_heap(tree->left) &&
 				binary_tree_is_heap(tree->right));
 	}
 	return (0);
 }
 
-
+/* CODE DOESN'T PASS ALL CHECKS */
